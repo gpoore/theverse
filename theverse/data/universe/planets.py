2b66a28f87f6662935827b3e9aafaa3b575c71c7
Geoffrey Poore <gpoore@gmail.com>
2020-06-11 16:16:04 -0500

    added planets Mercury and Venus

--- theverse/data/universe/planets.py
+++ theverse/data/universe/planets.py
@@ -12,6 +12,32 @@ from ...classes import Planet
 
 
 Planet(
+    name='Mercury',
+    reference='Williams, D.R. (27 September 2018). "Mercury Fact Sheet". NASA Goddard Space Flight Center.',
+    reference_url='https://nssdc.gsfc.nasa.gov/planetary/factsheet/mercuryfact.html',
+    planetary_system='Solar System',
+    primary='Sun',
+    mass='0.33011e24 kg',
+    equatorial_radius='2439.7 km',
+    polar_radius='2439.7 km',
+    volumetric_mean_radius='2439.7 km',
+)
+
+
+Planet(
+    name='Venus',
+    reference='Williams, D.R. (27 September 2018). "Venus Fact Sheet". NASA Goddard Space Flight Center.',
+    reference_url='https://nssdc.gsfc.nasa.gov/planetary/factsheet/venusfact.html',
+    planetary_system='Solar System',
+    primary='Sun',
+    mass='4.8675e24 kg',
+    equatorial_radius='6051.8 km',
+    polar_radius='6051.8 km',
+    volumetric_mean_radius='6051.8 km',
+)
+
+
+Planet(
     name='Earth',
     reference='Williams, D.R. (02 April 2020). "Earth Fact Sheet". NASA Goddard Space Flight Center.',
     reference_url='https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html',
